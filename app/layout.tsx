96f877813d4e24db21fe3648c76f69e7c61cd3e3
Callum Kloos <callumkloos@gmail.com>
2023-07-14 18:02:23 +0100

    clerk dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navigation/navbar";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export const metadata = {
   title: "Culture Collector",
@@ -9,7 +10,7 @@ export const metadata = {
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body className="flex min-h-screen flex-col items-center bg-background text-foreground">
           <Navbar />
