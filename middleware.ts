19dd9ae53aeaf44aa7877b3d9a1cae36f93dbe49
Callum Kloos <callumkloos@gmail.com>
2023-07-17 12:23:00 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // This protects all routes, including api routes
 export default authMiddleware({
-  publicRoutes: ["/", "/api/users"],
+	publicRoutes: ["/", "/api/users", "/api/collection"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
