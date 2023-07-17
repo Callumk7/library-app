a75fad5011c6f7cdf2bcb658ac76cbf961efbe13
Callum Kloos <callumkloos@gmail.com>
2023-07-17 12:22:38 +0100

    navbar

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
-import { cookies } from "next/headers";
 import Searchbar from "./searchbar";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 
 export default async function Navbar() {
   return (
     <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
       <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm text-foreground">
-        <Searchbar />
-        <div>
-          <UserButton afterSignOutUrl="/" />
+        <div className="flex flex-row items-center space-x-6">
+          <Searchbar />
+          <Link href="/collection" prefetch={false}>My Collection</Link>
         </div>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
       </div>
     </nav>
   );
