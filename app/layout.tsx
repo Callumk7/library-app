29e5c550f6f3e2b2dcdddbb6344024ce158c8e8d
Callum Kloos <callumkloos@gmail.com>
2023-07-20 09:41:01 +0100

    useState for collection

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/navigation/navbar";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import Providers from "@/lib/jotai/providers";
 
 export const metadata = {
   title: "Culture Collector",
@@ -13,12 +12,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <Providers>
           <body className="flex min-h-screen flex-col items-center bg-background text-foreground">
             <Navbar />
             {children}
           </body>
-        </Providers>
       </html>
     </ClerkProvider>
   );
