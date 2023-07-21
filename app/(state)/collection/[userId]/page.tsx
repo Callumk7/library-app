70562e8840062c2ee8cdc907b8d00077a19da82e
Callum Kloos <callumkloos@gmail.com>
2023-07-21 18:02:59 +0100

    removed suspense

--- app/(state)/collection/[userId]/page.tsx
+++ app/(state)/collection/[userId]/page.tsx
@@ -22,9 +22,7 @@ export default async function CollectionPage({ params }: { params: { userId: str
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between space-y-10 p-24">
-      <Suspense>
         <CollectionView collection={data} />
-      </Suspense>
     </main>
   );
 }
