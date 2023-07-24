b46969d07fa41a1ddefe1fa473dde903252b9f2d
Callum Kloos <callumkloos@gmail.com>
2023-07-24 12:37:57 +0100

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,21 @@ module.exports = {
 					from: { opacity: 0, transform: "translateX(-2px)" },
 					to: { opacity: 1, transform: "translateX(0)" },
 				},
+				// toast animations
+				hide: {
+					from: { opacity: 1 },
+					to: { opacity: 0 },
+				},
+				slideIn: {
+					from: {
+						transform: "translateX(calc(100% + var(--viewport-padding)))",
+					},
+					to: { transform: "translateX(0)" },
+				},
+				swipeOut: {
+					from: { transform: "translateX(var(--radix-toast-swipe-end-x))" },
+					to: { transform: "translateX(calc(100% + var(--viewport-padding)))" },
+				},
 			},
 			animation: {
 				slideDownAndFade: "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
@@ -44,6 +59,10 @@ module.exports = {
 				slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
 				slideRightAndFade:
 					"slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+				// toast animations
+				hide: "hide 100ms ease-in",
+				slideIn: "slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+				swipeOut: "swipeOut 100ms ease-out",
 			},
 		},
 	},
