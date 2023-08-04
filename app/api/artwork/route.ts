77d4a0a35e767f373e2936499671d0b82d253986
Callum Kloos <callumkloos@gmail.com>
2023-08-04 12:35:19 +0100

    Created secondary work handlers
    These routes handle creating secondary data for new games, genres, storylines, ratings.

---
+++ app/api/artwork/route.ts
@@ -0,0 +1,65 @@
+import { prisma } from "@/lib/prisma/client";
+import { Job } from "@/types";
+import { NextRequest, NextResponse } from "next/server";
+
+// shape of job is known
+export async function POST(req: NextRequest) {
+	console.log("artwork route hit!");
+
+	const job: Job = await req.json();
+	if (!job) {
+		return new NextResponse("No job provided", { status: 401 });
+	}
+
+	if (job.type !== "artwork") {
+		return new NextResponse("incorrect job type", { status: 401 });
+	}
+
+	if (!job.payload) {
+		return new NextResponse("No payload provided", { status: 401 });
+	}
+
+	if (!job.payload.artwork) {
+		return new NextResponse("payload contains no artwork to process", {
+			status: 401,
+		});
+	}
+
+	// payload for artork is an array of artwork and screenshots to process..
+	const promises = [];
+	for (const art of job.payload.artwork) {
+		if (art.type === "artwork") {
+			const upsertArtworkPromise = prisma.artwork.upsert({
+				where: {
+					imageId: art.image_id,
+				},
+				update: {},
+				create: {
+					imageId: art.image_id,
+					gameId: job.payload.gameId,
+				},
+			});
+
+			promises.push(upsertArtworkPromise);
+		}
+		if (art.type === "artwork") {
+			const upsertScreenshotPromise = prisma.screenshot.upsert({
+				where: {
+					imageId: art.image_id,
+				},
+				update: {},
+				create: {
+					imageId: art.image_id,
+					gameId: job.payload.gameId,
+				},
+			});
+
+			promises.push(upsertScreenshotPromise);
+		}
+	}
+
+	const results = await Promise.all(promises);
+	const resultsJson = JSON.stringify(results);
+	console.log("all artwork processed");
+	return new NextResponse(resultsJson, {status: 200});
+}
---
+++ app/api/genres/route.ts
@@ -0,0 +1,76 @@
+import { prisma } from "@/lib/prisma/client";
+import { Job } from "@/types";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+	console.log("genres route hit!");
+
+	const job: Job = await req.json();
+
+	if (!job) {
+		return new NextResponse("No job provided", { status: 401 });
+	}
+
+	if (job.type !== "genre") {
+		return new NextResponse("incorrect job type", { status: 401 });
+	}
+
+	if (!job.payload) {
+		return new NextResponse("No payload provided", { status: 401 });
+	}
+
+	if (!job.payload.genres) {
+		return new NextResponse("No genres provided in payload", { status: 401 });
+	}
+
+	const promises = [];
+	for (const genre of job.payload.genres) {
+		const upsertGenrePromise = prisma.genre.upsert({
+			where: {
+				externalId: genre.id,
+			},
+			update: {
+				games: {
+					connectOrCreate: {
+						where: {
+							gameId_genreId: {
+								gameId: job.payload.gameId,
+								genreId: genre.id,
+							},
+						},
+						create: {
+							gameId: job.payload.gameId,
+						},
+					},
+				},
+			},
+			create: {
+				externalId: genre.id,
+				name: genre.name,
+				games: {
+					connectOrCreate: {
+						where: {
+							gameId_genreId: {
+								gameId: job.payload.gameId,
+								genreId: genre.id,
+							},
+						},
+						create: {
+							gameId: job.payload.gameId,
+						},
+					},
+				},
+			},
+			select: {
+				id: true,
+			},
+		});
+
+		promises.push(upsertGenrePromise);
+	}
+
+	const results = await Promise.all(promises);
+	const returnJson = JSON.stringify(results);
+
+	return new NextResponse(returnJson, { status: 200 });
+}
