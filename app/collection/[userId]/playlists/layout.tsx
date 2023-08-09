0cc2b3a28c6c2e0b57e2509de2ba2ebf2e2f3451
Callum Kloos <callumkloos@gmail.com>
2023-08-09 18:24:36 +0100

    Add playlist route

---
+++ app/collection/[userId]/playlists/layout.tsx
@@ -0,0 +1,25 @@
+import { PlaylistNavBar } from "@/components/playlists/navbar";
+import { getPlaylists, getPlaylistsWithGames } from "@/lib/prisma/playlists/queries";
+import { auth } from "@clerk/nextjs";
+
+export default async function PlaylistLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { userId: string };
+}) {
+  const { userId } = auth();
+  if (userId !== params.userId) {
+    return <h1>NOT YOU, GET OUT</h1>;
+  }
+
+  const playlists = await getPlaylists(userId);
+
+  return (
+    <div>
+      <PlaylistNavBar playlists={playlists} />
+      {children}
+    </div>
+  );
+}
