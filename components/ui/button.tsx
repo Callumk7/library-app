9e59fd1c57b0cef50c5f9e759fb082af7f74257f
Callum Kloos <callumkloos@gmail.com>
2023-08-10 16:24:44 +0100

    Update Button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,14 +4,14 @@ import { clsx } from "clsx";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-dark hover:bg-primary/90",
-        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline: "border bg-background hover:bg-light/10",
-        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        default: "bg-primary text-primary-foreground hover:bg-primary/80",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "border bg-background hover:bg-foreground/5",
+        secondary: "bg-foreground text-background hover:bg-primary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         linkMono: "text-foreground underline-offset-4 hover:underline",
