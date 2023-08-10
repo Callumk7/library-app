1883eb278726e5d867e921300ddecc41691f0d84
Callum Kloos <callumkloos@gmail.com>
2023-08-10 10:11:59 +0100

    fix: mark isCompleted to false in playlist view

--- components/playlists/container.tsx
+++ components/playlists/container.tsx
@@ -14,7 +14,7 @@ export function PlaylistContainer({ games }: PlaylistContainerProps) {
   return (
     <div className="mx-auto grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {playlistState.map((game, index) => (
-        <GameCard key={index} game={game}>
+        <GameCard key={index} game={game} isCompleted={false} isStarred={false}>
           <div>controls will go here</div>
         </GameCard>
       ))}
