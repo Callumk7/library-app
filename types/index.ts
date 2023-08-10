e95fcde2b73664368ac1df5e972ccc5e2e5f2d5d
Callum Kloos <callumkloos@gmail.com>
2023-08-10 09:05:59 +0100

    notes on image dimensions

--- types/index.ts
+++ types/index.ts
@@ -188,6 +188,19 @@ type IGDBImage =
 	| "720p"
 	| "1080p";
 
+/* 
+cover_small			90 x 128 	Fit
+screenshot_med		569 x 320 	Lfill, Center gravity
+cover_big			264 x 374 	Fit
+logo_med			284 x 160 	Fit
+screenshot_big		889 x 500 	Lfill, Center gravity
+screenshot_huge 	1280 x 720 	Lfill, Center gravity
+thumb				90 x 90 	Thumb, Center gravity
+micro				35 x 35 	Thumb, Center gravity
+720p				1280 x 720 	Fit, Center gravity
+1080p				1920 x 1080 	Fit, Center gravity 
+*/
+
 export { IGDBGameSchema };
 export type { IGDBGame, IGDBImage };
 
