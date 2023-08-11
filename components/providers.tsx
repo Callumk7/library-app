83ce787411d026063f969e6c3604fca45051d57b
Callum Kloos <callumkloos@gmail.com>
2023-08-11 14:53:12 +0100

    Replace hovercard with tooltip

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,11 +2,14 @@ import { ToastProvider } from "./ui/toast";
 import { ReactNode } from "react";
 import { dark } from "@clerk/themes";
 import { ClerkProvider } from "@clerk/nextjs";
+import { TooltipProvider } from "./ui/tooltip";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
-      <ToastProvider>{children}</ToastProvider>
+      <ToastProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </ToastProvider>
     </ClerkProvider>
   );
 }
