205dff0da3591b6ba5268869f91da8acbdb81c55
Callum Kloos <callumkloos@gmail.com>
2023-08-11 18:24:08 +0100

    Game cover component tweaks

--- components/games/game-cover.tsx
+++ components/games/game-cover.tsx
@@ -1,9 +1,9 @@
 import { GameWithCoverAndGenres, IGDBImage } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
-import { Circle } from "rc-progress";
-import { Checkbox } from "@/components/ui/checkbox";
+import { Line } from "rc-progress";
 import clsx from "clsx";
+import { Tag } from "../ui/tag";
 
 interface CollectionItemProps {
   game: GameWithCoverAndGenres;
@@ -24,13 +24,16 @@ export function GameCard({
   if (isCompleted) {
     borderStyle =
       "border border-lime-500/40 hover:border-lime-500 shadow-lg shadow-lime-500/20 hover:shadow-lime-500/40";
+  } else if (game.aggregatedRating !== null && game.aggregatedRating > 95) {
+    borderStyle =
+      "border border-orange-500/40 hover:border-orange-500 shadow-lg shadow-orange-500/20 hover:shadow-orange-500/40";
   }
 
   return (
     <div
       className={clsx(
         borderStyle,
-        "relative flex max-w-sm flex-col justify-between overflow-hidden rounded-lg text-foreground"
+        "relative flex max-w-sm flex-col items-center justify-between overflow-hidden rounded-lg text-foreground"
       )}
     >
       <Link
@@ -43,11 +46,11 @@ export function GameCard({
           width={720}
           height={1280}
         />
-        <Circle
-          percent={game.aggregatedRating!}
-          strokeWidth={8}
+        <Line
+          percent={game.aggregatedRating ? game.aggregatedRating : 0}
+          strokeWidth={2}
           strokeColor="#F0F757"
-          className="absolute left-2 top-2 h-10 w-10"
+          trailColor=""
         />
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-80 opacity-0 transition ease-in-out group-hover:opacity-100">
           <div className="animate-pulse text-center text-4xl text-white">
@@ -55,14 +58,9 @@ export function GameCard({
           </div>
         </div>
       </Link>
-      <div className="m-2 flex flex-wrap gap-1">
+      <div className="m-2 flex flex-wrap gap-2">
         {game.genres.map((genre, index) => (
-          <p
-            className="inline rounded-md bg-gray-800 px-2 py-1 text-[10px] text-light/70"
-            key={index}
-          >
-            {genre.genre.name}
-          </p>
+          <Tag key={index}>{genre.genre.name}</Tag>
         ))}
       </div>
       {children}
