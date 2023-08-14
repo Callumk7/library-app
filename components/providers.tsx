60e364e888e9ce1722aace828d4665116bb18db1
Callum Kloos <callumkloos@gmail.com>
2023-08-14 17:03:48 +0100

    Separate client and server providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,15 +1,11 @@
-import { ToastProvider } from "./ui/toast";
-import { ReactNode } from "react";
 import { dark } from "@clerk/themes";
 import { ClerkProvider } from "@clerk/nextjs";
-import { TooltipProvider } from "./ui/tooltip";
+import { ClientProviders } from "./providers.client";
 
-export function Providers({ children }: { children: ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
-      <ToastProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </ToastProvider>
+      <ClientProviders>{children}</ClientProviders>
     </ClerkProvider>
   );
 }
