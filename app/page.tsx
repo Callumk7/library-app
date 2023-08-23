043d742995456f72c3e9b85126477477b3b3bc7b
Callum Kloos <callumkloos@gmail.com>
2023-08-23 15:52:34 +0100

    prisma issues with neon

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import { GameCardCover } from "@/components/games/GameCardCover";
 import { getTopRatedGames } from "@/features/dashboard/queries/prisma-functions";
 
-export default async function HomePage() {
-  const games = await getTopRatedGames(30);
+export default function HomePage() {
   return (
-    <main className="mx-auto grid w-4/5 grid-cols-2 gap-5 my-10 md:grid-cols-3">
-      {games.map((game, index) => (
-        <GameCardCover key={index} game={game} isCompleted={false}>
-          <div>controls</div>
-        </GameCardCover>
-      ))}
+    <main className="mx-auto my-10 grid w-4/5 grid-cols-2 gap-5 md:grid-cols-3">
+      <div>main</div>
     </main>
   );
 }
