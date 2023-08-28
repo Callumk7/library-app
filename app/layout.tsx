6076a7eda71f9782781b505f9bc84e0294e0ba97
Callum Kloos <callumkloos@gmail.com>
2023-08-28 09:53:28 +0100

    Add poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClientProviders } from "@/components/ClientProviders";
 import "./globals.css";
 import { ToastViewport } from "@/components/ui/toast";
 import Navbar from "@/features/navigation/components/Navbar";
+import { inter, poppins } from "@/style/fonts";
 
 export const metadata = {
   title: "playQ",
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClientProviders>
-      <html lang="en">
+      <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
         <body className="flex min-h-screen flex-col items-center bg-background pt-3 text-foreground">
           <Navbar />
           {children}
