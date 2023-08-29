c5c87fc411e2d74e070ccffd1a95845baea8a9af
Callum Kloos <callumkloos@gmail.com>
2023-08-29 12:34:26 +0100

    add pathname to navbar

--- features/navigation/components/Navbar.tsx
+++ features/navigation/components/Navbar.tsx
@@ -1,22 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { UserProfile } from "./UserProfile";
 import { Playlist } from "@/components/ui/icons/Playlist";
 import { Home } from "@/components/ui/icons/Home";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathName = usePathname();
   const userId = "user_2Tmlvj4Ju83ZYElhXRg9pNjvakf";
   return (
     <nav className="flex h-16 w-full justify-between border">
       <div className="mx-auto flex w-4/5 items-center justify-between p-3 text-sm text-foreground">
         <div className="flex flex-row space-x-3 align-middle">
-          <Button asChild variant={"ghost"} size={"sm"}>
+          <Button asChild variant={pathName === "/" ? "secondary" : "ghost"} size={"sm"}>
             <Link href={`/`}>
               <Home className="mr-2" />
               <span>Home</span>
             </Link>
           </Button>
-          <Button asChild variant={"ghost"} size={"sm"}>
+          <Button asChild variant={pathName.startsWith("/collection") ? "secondary" : "ghost"} size={"sm"}>
             <Link href={`/collection/${userId}`}>
               <Playlist className="mr-2" />
               <span>My Collection</span>
