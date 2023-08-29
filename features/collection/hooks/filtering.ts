30092c2d660744345a8923a50bc5e0a944686dad
Callum Kloos <callumkloos@gmail.com>
2023-08-29 11:18:36 +0100

    remove unused import

--- features/collection/hooks/filtering.ts
+++ features/collection/hooks/filtering.ts
@@ -1,4 +1,4 @@
-import { GameWithCoverAndGenres, GameWithCoverGenresPlaylists, SortOption } from "@/types";
+import { GameWithCoverAndGenres, SortOption } from "@/types";
 import { applySorting } from "@/util/sorting";
 import { useMemo, useState } from "react";
 
