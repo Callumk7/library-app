e803ccafbb453da545c2221a451b148f59bb50ca
Callum Kloos <callumkloos@gmail.com>
2023-08-29 11:19:09 +0100

    Build temporary inline game item

---
+++ components/games/GameListEntry.tsx
@@ -0,0 +1,24 @@
+import { GameWithCoverGenresPlaylists } from "@/types";
+
+interface GameListEntryProps {
+  game: GameWithCoverGenresPlaylists;
+  children: React.ReactNode;
+}
+export function GameListEntry({ game, children }: GameListEntryProps) {
+  return (
+    <div className="relative flex w-full flex-row justify-between border p-3">
+      <div className="flex flex-col space-y-1">
+        <p className="text-xs font-bold">{game.title}</p>
+        <p className="text-xs font-bold">
+          <span className="text-red-300">rating: </span>
+          {game.aggregatedRating}
+        </p>
+        <p className="text-xs font-bold">
+          <span className="text-red-300">release date: </span>
+          {game.releaseDate}
+        </p>
+      </div>
+      {children}
+    </div>
+  );
+}
