7a04629b8efd9b505673b803376364a251b71117
Callum Kloos <callumkloos@gmail.com>
2023-08-29 12:28:12 +0100

    remove search from main navbar

--- features/navigation/components/Navbar.tsx
+++ features/navigation/components/Navbar.tsx
@@ -1,25 +1,29 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Searchbar from "./Searchbar";
 import { UserProfile } from "./UserProfile";
+import { Playlist } from "@/components/ui/icons/Playlist";
+import { Home } from "@/components/ui/icons/Home";
 
 export default function Navbar() {
   const userId = "user_2Tmlvj4Ju83ZYElhXRg9pNjvakf";
   return (
-    <nav className="flex h-16 w-full justify-center border">
-      <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm text-foreground">
-        <div className="flex flex-row items-center space-x-6">
+    <nav className="flex h-16 w-full justify-between border">
+      <div className="mx-auto flex w-4/5 items-center justify-between p-3 text-sm text-foreground">
+        <div className="flex flex-row space-x-3 align-middle">
           <Button asChild variant={"ghost"} size={"sm"}>
-            <Link href={`/`}>Home</Link>
+            <Link href={`/`}>
+              <Home className="mr-2" />
+              <span>Home</span>
+            </Link>
           </Button>
-          <Button asChild variant={"secondary"} size={"sm"}>
-            <Link href={`/collection/${userId}`}>My Collection</Link>
+          <Button asChild variant={"ghost"} size={"sm"}>
+            <Link href={`/collection/${userId}`}>
+              <Playlist className="mr-2" />
+              <span>My Collection</span>
+            </Link>
           </Button>
-          <Searchbar />
-          <UserProfile />
         </div>
+        <UserProfile />
       </div>
     </nav>
   );
