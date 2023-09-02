58e3527327cf87b0854929ac8f398248a3123b51
Callum Kloos <callumkloos@gmail.com>
2023-09-02 13:05:55 +0100

    force cache on fetch

--- util/igdb.ts
+++ util/igdb.ts
@@ -48,6 +48,7 @@ export async function getSearchResults(q: string): Promise<IGDBGame[]> {
 			"content-type": "text/plain",
 		},
 		body: `search "${q}"; fields name, artworks.image_id, screenshots.image_id, aggregated_rating, aggregated_rating_count, cover.image_id, storyline, first_release_date, genres.name; limit 40; where artworks != null & cover.image_id != null;`,
+		cache: "force-cache"
 	});
 
 	if (!res.ok) {
